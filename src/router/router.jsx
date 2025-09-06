29a9ce53053eb42ba1a44f9b93e1cf34b4097c35
mdasifalijihat <mdasifalijihat@gmail.com>
2025-09-06 17:15:56 +0600

    added router page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,13 +1,24 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../Layout/MainLayout";
+import Error from "../components/pages/Error/Error";
 import Home from "../components/shared/Home/Home";
+import About from "../components/pages/page/About";
+import Contact from "../components/pages/page/Contact";
+import Login from "../components/pages/login/Login";
+import Register from "../components/pages/login/Register";
+
+
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    errorElement: <div>404 Not Found</div>,
-    children:[
-      {index:true, Component:Home}
-    ]
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Home /> },       
+      { path: "about", element: <About /> },   
+      { path: "contact", element: <Contact /> },
+      { path: "login", element: <Login /> },    
+      { path: "register", element: <Register /> }, 
+    ],
   },
 ]);
